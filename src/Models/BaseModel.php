7a8cf99e2931c76b5d547344bdb7c5941d52d728
Guilherme Pressutto <gpressutto5@gmail.com>
2017-04-28 12:12:45 -0300

    Make tests green again

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -6,7 +6,7 @@ abstract class BaseModel
     public function __construct($attributes)
     {
         foreach ($attributes as $key => $value) {
-            if (!property_exists(self, $key)) {
+            if (!property_exists($this, $key)) {
                 continue;
             }
 
