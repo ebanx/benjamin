1338de1c3be7ebe8f28f47d7cfb9db26efc56794
Guilherme Pressutto <gpressutto5@gmail.com>
2017-04-28 14:48:29 -0300

    Commented BaseModel

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -3,6 +3,11 @@ namespace Ebanx\Benjamin\Models;
 
 abstract class BaseModel
 {
+    /**
+     * Fill the object with the provided $attributes array
+     *
+     * @param $attributes array
+     */
     public function __construct($attributes)
     {
         foreach ($attributes as $key => $value) {
