eb514252dd5a91eee92243ac9f4030625e1c9e36
Guilherme Pressutto <gpressutto5@gmail.com>
2017-04-28 19:24:24 -0300

    Commented Person

--- src/Models/Person.php
+++ src/Models/Person.php
@@ -6,11 +6,53 @@ class Person extends BaseModel
     const TYPE_PERSONAL = "personal";
     const TYPE_BUSINESS = "business";
 
+    /**
+     * The type of customer.
+     * Supported person types: 'personal' and 'business'.
+     *
+     * @var string
+     */
     public $personType = self::TYPE_PERSONAL;
+
+    /**
+     * Customers birthdate.
+     *
+     * @var \DateTime
+     */
     public $birthdate;
+
+    /**
+     * Customers document.
+     *
+     * @var string
+     */
     public $document;
+
+    /**
+     * Customers email.
+     *
+     * @var string
+     */
     public $email;
+
+    /**
+     * Customers IP.
+     *
+     * @var string
+     */
     public $ip;
+
+    /**
+     * Customers name.
+     *
+     * @var string
+     */
     public $name;
+
+    /**
+     * Customers phone number.
+     *
+     * @var string
+     */
     public $phoneNumber;
 }
