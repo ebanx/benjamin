057fd87d0ed7c2094d46df633e27817fcf1b447a
Guilherme <gpressutto5@gmail.com>
2017-05-02 13:07:48 -0300

    Arrays is optional for base model constructor

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -8,7 +8,7 @@ abstract class BaseModel
      *
      * @param $attributes array
      */
-    public function __construct($attributes)
+    public function __construct($attributes = array())
     {
         foreach ($attributes as $key => $value) {
             if (!property_exists($this, $key)) {
