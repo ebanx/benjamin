0395cafab92cc00e047b529c4af45f5c723a2431
SparK-Cruz <spark.crz@gmail.com>
2017-05-03 17:04:24 -0300

    Added notification url to config

--- src/Models/Configs/Config.php
+++ src/Models/Configs/Config.php
@@ -9,52 +9,59 @@ class Config extends BaseModel
     const IOF = 0.0038;
 
     /**
-     * Live integration key
+     * Live integration key.
      *
      * @var string
      */
     public $integrationKey;
 
     /**
-     * Public live integration key
+     * Public live integration key.
      *
      * @var string
      */
     public $publicIntegrationKey;
 
     /**
-     * Sandbox integration key
+     * Sandbox integration key.
      *
      * @var string
      */
     public $sandboxIntegrationKey;
 
     /**
-     * Public sandbox integration key
+     * Public sandbox integration key.
      *
      * @var string
      */
     public $publicSandboxIntegrationKey;
 
     /**
-     * Determines if connection should be made using the sandbox environment settings
+     * Determines if connection should be made using the sandbox environment settings.
      *
      * @var bool
      */
     public $isSandbox = true;
 
     /**
-     * Sets the site default currency ISO code
-     * BRL, USD, EUR, COP, MXN, CLP, PEN
+     * Sets the site default currency ISO code.
+     * (BRL, USD, EUR, COP, MXN, CLP, PEN)
      *
      * @var string
      */
     public $baseCurrency = Currency::USD;
 
     /**
-     * IOF Brazilian tax
+     * IOF Brazilian tax.
      *
      * @var float
      */
     public $iof = self::IOF;
+
+    /**
+     * The URL to send notifications to.
+     *
+     * @var string
+     */
+    public $notificationUrl = null;
 }
