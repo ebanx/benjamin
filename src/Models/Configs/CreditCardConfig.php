ccb2d6236344b9f0e7c8e3344b2633533cc40247
SparK-Cruz <spark.crz@gmail.com>
2017-05-03 17:11:09 -0300

    DocComment format

--- src/Models/Configs/CreditCardConfig.php
+++ src/Models/Configs/CreditCardConfig.php
@@ -8,29 +8,29 @@ class CreditCardConfig extends BaseModel
     const MAX_INSTALMENTS = 12;
 
     /**
-     * Number of max instalments, defaults to 12
+     * Number of max instalments, defaults to 12.
      *
      * @var integer
      */
     public $maxInstalments = self::MAX_INSTALMENTS;
 
     /**
-     * Minimum instalment amount
-     * Default varies by currency
+     * Minimum instalment amount.
+     * Default varies by currency.
      *
      * @var float
      */
     public $minInstalmentAmount = 0.0;
 
     /**
-     * List of interest rate config objects
+     * List of interest rate config objects.
      *
      * @var array
      */
     public $interestRates = array();
 
     /**
-     * Adds an interest rate config object for the credit card config
+     * Adds an interest rate config object for the credit card config.
      *
      * @param integer $instalmentNumber The instalment number for this rate configuration
      * @param float   $rate              The interest rate to be applied
