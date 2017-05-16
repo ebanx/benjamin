fd0d835107e657cc7d80c25f17642594a410ebf0
Guilherme Pressutto <gpressutto5@gmail.com>
2017-05-16 15:26:27 -0300

    Request adapter passing

--- src/Services/Adapters/RequestAdapter.php
+++ src/Services/Adapters/RequestAdapter.php
@@ -6,8 +6,15 @@ use Ebanx\Benjamin\Models\Payment;
 
 abstract class RequestAdapter
 {
-    private $payment;
+    protected $payment;
     private $config;
+    private $countryCode = array(
+        'brasil' => 'br',
+        'peru' => 'pe',
+        'mexico' => 'mx',
+        'colombia' => 'co',
+        'chile' => 'cl'
+    );
 
     public function __construct(Payment $payment, Config $config)
     {
@@ -26,7 +33,6 @@ abstract class RequestAdapter
             'integration_key' => $this->getIntegrationKey(),
             'operation' => 'request',
             'mode' => 'full',
-            'person_type' => $this->payment->person->type,
             'payment' => $this->transformPayment()
         );
     }
@@ -34,7 +40,22 @@ abstract class RequestAdapter
     protected function transformPayment()
     {
         return (object) array(
-
+            'name' => $this->payment->person->name,
+            'email' => $this->payment->person->email,
+            'currency_code' => $this->payment->currencyCode,
+            'amount_total' => $this->payment->amountTotal,
+            'merchant_payment_code' => $this->payment->merchantPaymentCode,
+            'birth_date' => $this->payment->person->birthdate->format('d/m/Y'),
+            'customer_ip' => $this->payment->person->ip,
+            'zipcode' => $this->payment->address->zipcode,
+            'address' => $this->payment->address->address,
+            'street_number' => $this->payment->address->streetNumber,
+            'street_complement' => $this->payment->address->streetComplement,
+            'city' => $this->payment->address->city,
+            'state' => $this->payment->address->state,
+            'country' => $this->countryCode[strtolower($this->payment->address->country)],
+            'phone_number' => $this->payment->person->phoneNumber,
+            // TODO: User Value 1-5
         );
     }
 }
