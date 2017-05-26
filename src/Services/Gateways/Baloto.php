a417b86df17dbbe133bbdd5d3ada7f432d83c65b
SparK-Cruz <spark.crz@gmail.com>
2017-05-25 22:25:30 -0300

    Added mandatory abstract methods for availability check

--- src/Services/Gateways/Baloto.php
+++ src/Services/Gateways/Baloto.php
@@ -1,11 +1,26 @@
 <?php
 namespace Ebanx\Benjamin\Services\Gateways;
 
+use Ebanx\Benjamin\Models\Country;
+use Ebanx\Benjamin\Models\Currency;
 use Ebanx\Benjamin\Models\Payment;
 use Ebanx\Benjamin\Services\Adapters\CashRequestAdapter;
 
 class Baloto extends BaseGateway
 {
+    protected function getEnabledCountries()
+    {
+        return array(Country::COLOMBIA);
+    }
+    protected function getEnabledCurrencies()
+    {
+        return array(
+            Currency::COP,
+            Currency::USD,
+            Currency::EUR
+        );
+    }
+
     public function create(Payment $payment)
     {
         $payment->type = "baloto";
--- src/Services/Gateways/Oxxo.php
+++ src/Services/Gateways/Oxxo.php
@@ -1,11 +1,26 @@
 <?php
 namespace Ebanx\Benjamin\Services\Gateways;
 
+use Ebanx\Benjamin\Models\Country;
+use Ebanx\Benjamin\Models\Currency;
 use Ebanx\Benjamin\Models\Payment;
 use Ebanx\Benjamin\Services\Adapters\CashRequestAdapter;
 
 class Oxxo extends BaseGateway
 {
+    protected function getEnabledCountries()
+    {
+        return array(Country::MEXICO);
+    }
+    protected function getEnabledCurrencies()
+    {
+        return array(
+            Currency::MXN,
+            Currency::USD,
+            Currency::EUR
+        );
+    }
+
     public function create(Payment $payment)
     {
         $payment->type = "oxxo";
