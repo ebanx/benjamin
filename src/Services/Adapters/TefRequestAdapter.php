4704c826bd6418826e7e221ec283b1a93f53d75b
Guilherme Pressutto <gpressutto5@gmail.com>
2017-05-26 21:25:52 -0300

    Removed bypass boleto screen

--- src/Services/Adapters/TefRequestAdapter.php
+++ src/Services/Adapters/TefRequestAdapter.php
@@ -6,7 +6,6 @@ class TefRequestAdapter extends BrazilRequestAdapter
     public function transform()
     {
         $transformed = parent::transform();
-        $transformed->bypass_boleto_screen = true;
 
         return $transformed;
     }
