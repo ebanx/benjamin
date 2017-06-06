fd60071664dff7f51f3a9f4e5c878b841583fe9b
Guilherme Pressutto <gpressutto5@gmail.com>
2017-06-06 16:16:16 -0300

    Oxxo getUrl

--- src/Services/Gateways/Oxxo.php
+++ src/Services/Gateways/Oxxo.php
@@ -32,4 +32,13 @@ class Oxxo extends BaseGateway
 
         return $body;
     }
+
+    /**
+     * @param string $hash
+     * @return string
+     */
+    public function getUrl($hash)
+    {
+        return 'https://sandbox.ebanx.com/print/oxxo/?hash=' . $hash;
+    }
 }
