dff229d99ab173ca77d1c3a7296c83888a8a43f3
Anderson Campanha <anderson.campanha@ebanx.com>
2017-08-21 13:42:17 -0300

    Added webpay request adapter

---
+++ tests/Unit/Services/Adapters/WebpayRequestAdapterTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Tests\Unit\Services\Adapters;
+
+use Ebanx\Benjamin\Services\Adapters\WebpayRequestAdapter;
+use Tests\Helpers\Builders\BuilderFactory;
+use JsonSchema;
+use Ebanx\Benjamin\Models\Configs\Config;
+
+class WebpayRequestAdapterTest extends RequestAdapterTest {
+    public function testJsonSchema() {
+        $config  = new Config( [
+            'sandboxIntegrationKey' => 'testIntegrationKey'
+        ] );
+        $factory = new BuilderFactory( 'pt_BR' );
+        $payment = $factory->payment()->build();
+
+        $adapter = new WebpayRequestAdapter( $payment, $config );
+        $result  = $adapter->transform();
+
+        $validator = new JsonSchema\Validator;
+        $validator->validate( $result, $this->getSchema( [ 'flowRequestSchema' ] ) );
+
+        $this->assertTrue( $validator->isValid(), $this->getJsonMessage( $validator ) );
+    }
+}
