81400e63f2ac0be377e581f2c3150dc10320d967
Egon Braun <egonbraun@gmail.com>
2017-08-21 16:42:35 +0000

    Apply fixes from StyleCI

--- tests/Unit/Services/Adapters/WebpayRequestAdapterTest.php
+++ tests/Unit/Services/Adapters/WebpayRequestAdapterTest.php
@@ -6,20 +6,22 @@ use Tests\Helpers\Builders\BuilderFactory;
 use JsonSchema;
 use Ebanx\Benjamin\Models\Configs\Config;
 
-class WebpayRequestAdapterTest extends RequestAdapterTest {
-    public function testJsonSchema() {
-        $config  = new Config( [
+class WebpayRequestAdapterTest extends RequestAdapterTest
+{
+    public function testJsonSchema()
+    {
+        $config  = new Config([
             'sandboxIntegrationKey' => 'testIntegrationKey'
-        ] );
-        $factory = new BuilderFactory( 'pt_BR' );
+        ]);
+        $factory = new BuilderFactory('pt_BR');
         $payment = $factory->payment()->build();
 
-        $adapter = new WebpayRequestAdapter( $payment, $config );
+        $adapter = new WebpayRequestAdapter($payment, $config);
         $result  = $adapter->transform();
 
         $validator = new JsonSchema\Validator;
-        $validator->validate( $result, $this->getSchema( [ 'flowRequestSchema' ] ) );
+        $validator->validate($result, $this->getSchema([ 'flowRequestSchema' ]));
 
-        $this->assertTrue( $validator->isValid(), $this->getJsonMessage( $validator ) );
+        $this->assertTrue($validator->isValid(), $this->getJsonMessage($validator));
     }
 }
