8b07ef5fb2b7be21f0fa58967f32a5fd8c92c269
Anderson Campanha <anderson.campanha@ebanx.com>
2018-02-05 19:17:00 -0200

    Leave bypass boleto screen only in request adapter

--- src/Services/Adapters/BoletoPaymentAdapter.php
+++ src/Services/Adapters/BoletoPaymentAdapter.php
@@ -6,7 +6,6 @@ class BoletoPaymentAdapter extends BrazilPaymentAdapter
     public function transformPayment()
     {
         $transformed = parent::transformPayment();
-        $transformed->bypass_boleto_screen = true;
 
         if (isset($this->payment->dueDate)) {
             $transformed->due_date = $this->payment->dueDate->format('d/m/Y');
--- src/Services/Adapters/CashPaymentAdapter.php
+++ src/Services/Adapters/CashPaymentAdapter.php
@@ -6,7 +6,6 @@ class CashPaymentAdapter extends PaymentAdapter
     public function transformPayment()
     {
         $transformed = parent::transformPayment();
-        $transformed->bypass_boleto_screen = true;
 
         if (isset($this->payment->dueDate)) {
             $transformed->due_date = $this->payment->dueDate->format('d/m/Y');
