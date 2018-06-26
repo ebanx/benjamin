8a6c091bc5b084da69d0d01ffe822ef8e7feb880
Anderson Campanha <anderson.campanha@ebanx.com>
2018-06-26 11:46:08 -0300

    Change EchoEngine to reflect guzzleless engine

--- tests/Helpers/Mocks/Http/EchoEngine.php
+++ tests/Helpers/Mocks/Http/EchoEngine.php
@@ -1,12 +1,9 @@
 <?php
 namespace Tests\Helpers\Mocks\Http;
 
-use GuzzleHttp\Client as Guzzle;
-use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\Message\Response;
-use Tests\Helpers\Providers\Request;
+use Ebanx\Benjamin\Services\Http\Engine;
 
-class EchoEngine extends Guzzle
+class EchoEngine extends Engine
 {
     /**
      * @var string
@@ -37,9 +34,11 @@ class EchoEngine extends Guzzle
     }
 
     /**
-     * @param  string $url     Url to request from
-     * @param  array|object  $options
+     * @param  string $url Url to request from
+     * @param  array|object $options
+     *
      * @return EchoEngineResponse
+     * @throws \Exception
      */
     public function post($url = null, $options = [])
     {
@@ -47,15 +46,23 @@ class EchoEngine extends Guzzle
     }
 
     /**
-     * @param  string $url     Url to request from
-     * @param  array  $options
+     * @param  string $url Url to request from
+     * @param  array $options
+     *
      * @return EchoEngineResponse
+     * @throws \Exception
      */
     public function get($url = null, $options = [])
     {
         return $this->fakeResponse($url);
     }
 
+    /**
+     * @param $url
+     *
+     * @return mixed|EchoEngineResponse|EchoEngineResponse[]
+     * @throws \Exception
+     */
     private function fakeResponse($url)
     {
         if (!is_array($this->responses)) {
@@ -64,10 +71,10 @@ class EchoEngine extends Guzzle
 
         $endpoint = str_replace($this->baseUrl, '', $url);
         if ($this->responses[$endpoint]->json()['status'] === 'CONFLICT') {
-            throw new ClientException('Conflict: invalid key', new \GuzzleHttp\Message\Request('GET', $endpoint), new Response(409));
+            throw new \Exception('Conflict: invalid key', 409);
         }
         if ($this->responses[$endpoint]->json()['status'] === 'NOT FOUND') {
-            throw new ClientException('Any other http status returned', new \GuzzleHttp\Message\Request('GET', $endpoint), new Response(404));
+            throw new \Exception('Any other http status returned', 404);
         }
         return $this->responses[$endpoint];
     }
@@ -96,20 +103,6 @@ class EchoEngineResponse
         return json_decode($this->response, true);
     }
 
-    public function getBody()
-    {
-        return new EchoEngineResponseBody($this->response);
-    }
-}
-
-class EchoEngineResponseBody
-{
-    private $response;
-
-    public function __construct($response)
-    {
-        $this->response = $response;
-    }
     public function getContents()
     {
         return $this->response;
